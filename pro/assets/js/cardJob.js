7da0d957d3ff33887dc36b9ebc9eeb2655a361f1
陳怡安 <lohaslife.debbie@gmail.com>
2023-02-08 19:45:02 +0800

    2023.02.08 final fixed footer

--- pro/assets/js/cardJob.js
+++ pro/assets/js/cardJob.js
@@ -1,2 +1,20 @@
-new Swiper(".cardSwiper",{slidesPerView:1,spaceBetween:30,loopFillGroupWithBlank:!0,autoplay:{delay:5e3},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************!*\
+  !*** ./src/js/card-job.js ***!
+  \****************************/
+var swiper = new Swiper(".cardSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loopFillGroupWithBlank: true,
+  autoplay: {
+    delay: 5000
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+/******/ })()
+;
 //# sourceMappingURL=cardJob.js.map
--- pro/assets/js/img.js
+++ pro/assets/js/img.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
@@ -534,7 +533,4 @@ __webpack_require__.r(__webpack_exports__);
 
 /******/ })()
 ;
-=======
-(()=>{"use strict";var t={};t.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"==typeof window)return window}}(),(()=>{var r;t.g.importScripts&&(r=t.g.location+"");var p=t.g.document;if(!r&&p&&(p.currentScript&&(r=p.currentScript.src),!r)){var e=p.getElementsByTagName("script");e.length&&(r=e[e.length-1].src)}if(!r)throw new Error("Automatic publicPath is not supported in this browser");r=r.replace(/#.*$/,"").replace(/\?.*$/,"").replace(/\/[^\/]+$/,"/"),t.p=r+"../../"})(),t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p,t.p})();
->>>>>>> yian
 //# sourceMappingURL=img.js.map
